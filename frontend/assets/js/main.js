483396638c22e6c09aaceca5c56c587d976827cf
shippc <wilhanmax@hotmail.com>
2023-04-02 17:02:40 -0300

    Cart

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -99,3 +99,24 @@ const scrollUp = () =>{
 						: scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
+
+/*=============== SHOW CART ===============*/
+const cart = document.getElementById('cart'),
+      cartShop = document.getElementById('cart-shop'),
+      cartClose = document.getElementById('cart-close')
+
+/*===== CART SHOW =====*/
+/* Validate if constant exists */
+if(cartShop){
+    cartShop.addEventListener('click', () =>{
+        cart.classList.add('show-cart')
+    })
+}
+
+/*===== CART HIDDEN =====*/
+/* Validate if constant exists */
+if(cartClose){
+    cartClose.addEventListener('click', () =>{
+        cart.classList.remove('show-cart')
+    })
+}
